f4f41c8cf1408ad37932b3b4cfde65574a069c3a
Kiosion <hi@kio.dev>
2021-09-12 19:02:49 -0300

    Update fish config, gitignore

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,53 @@
+#
+#________/\\\\\_____________________/\\\_________        
+# ______/\\\///_____________________\/\\\_________       
+#  _____/\\\_______/\\\______________\/\\\_________      
+#   __/\\\\\\\\\___\///___/\\\\\\\\\\_\/\\\_________     
+#    _\////\\\//_____/\\\_\/\\\//////__\/\\\\\\\\\\__    
+#     ____\/\\\______\/\\\_\/\\\\\\\\\\_\/\\\/////\\\_   
+#      ____\/\\\______\/\\\_\////////\\\_\/\\\___\/\\\_  
+#       ____\/\\\______\/\\\__/\\\\\\\\\\_\/\\\___\/\\\_ 
+#        ____\///_______\///__\//////////__\///____\///__
+#
+# Configuration options for fish shell
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Set path
-set PATH "$HOME/Library/Application Support/Übersicht/widgets/landscape/landscape" "/usr/local/bin/landscape" $PATH
+# configure display
+set -g theme_display_time                     yes
+set -g theme_display_group                    no
+set -g theme_display_git                      yes
+set -g theme_display_virtualenv               yes
+set -g theme_display_time_format              '+[%H:%M:%S]'
 
-# Configure omf theme
-set -g theme_display_time yes
-set -g theme_display_group no
-set -g theme_color_time brblack
-set -g theme_display_time_format '+[%H:%M:%S]'
+# config colours
+set -g theme_color_time                       brcyan
+set -g theme_color_user                       white
+set -g theme_color_separator                  white
+set -g theme_color_host                       brgreen
+set -g theme_color_bracket                    white
+set -g theme_color_normal                     white
+set -g theme_color_error                      brred
+set -g theme_color_superuser                  brred
+set -g theme_color_path                       brcyan
+set -g theme_color_rw                         white
+set -g theme_color_virtualenv                 bryellow
 
-# Load pyenv automatically
-status is-interactive; and pyenv init --path | source
-pyenv init - | source
-fish_add_path /usr/local/sbin
+set -g theme_prompt_char_normal               '$'
+set -g theme_prompt_chat_superuser            '#'
+set -g theme_prompt_char                      "$theme_prompt_chat_normal"
+
+# git config
+set -g __fish_git_prompt_char_branch_begin    '['
+set -g __fish_git_prompt_char_branch_end      ']'
+set -g __fish_git_prompt_color_branch_begin   white
+set -g __fish_git_prompt_color_branch_end     white
+set -g __fish_git_prompt_color_branch         bryellow
+
+
+# load pyenv automatically
+# status is-interactive; and pyenv init --path | source
+# pyenv init - | source
+# fish_add_path /usr/local/sbin
