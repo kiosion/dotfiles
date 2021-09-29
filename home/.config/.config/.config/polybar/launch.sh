20c6928f074e004d8ae9ccb6390bdb60bbcf52cf
kiosion <hi@kio.dev>
2021-09-28 21:14:45 -0300

    Remove .config

--- home/.config/.config/.config/polybar/launch.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-killall -q polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar wide 2>&1 | tee -a /tmp/polybar1.log & disown
