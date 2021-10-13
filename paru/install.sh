6370792082ea27f8d0273635a3f50c696d0e327d
Maxim <hi@kio.dev>
2021-10-13 20:15:49 -0300

    Update install.sh

--- paru/install.sh
+++ paru/install.sh
@@ -3,6 +3,6 @@
 echo "==> Installing paru..."
 sudo pacman -S --needed base-devel -y
 cd ~/pkgs
-git clone https://aur.archlinux.org/paru.git
-cd paru
+git clone https://aur.archlinux.org/paru.git ~/pkgs/paru
+cd ~/pkgs/paru
 makepkg -si
