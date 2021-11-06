e6ab86f2bf8b78d398222054922839845e92a147
Maxim <hi@kio.dev>
2021-11-06 12:27:59 -0300

    Update install.sh

--- system/install.sh
+++ system/install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 # Move fstab into place
-mv fstab /etc/fstab
+sudo mv fstab /etc/fstab
+
+# Move systemd overrides into place
+sudo mkdir /etc/systemd/system/getty@tty1.service.d && sudo mv systemd/getty@tty1.service.d/* /etc/systemd/system/getty@tty1.service.d/*
