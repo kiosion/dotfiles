a567fa662fee1023f7e8bb6b80174e12d08b770f
kiosion <hi@kio.dev>
2021-12-30 00:34:12 -0400

    alskhdghsjkasdlfjalskj;dfadskghjl

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -14,40 +14,45 @@
 # set ssh agent correctly
 fish_ssh_agent
 
+# greeter cmd for new windows
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    echo "" && echo "Welcome, "(whoami)"! | Arch "(uname -sr)"" && echo ""
+    #echo "" && echo "Mem: "(free -g | sed -n 's/.*Mem://p' | awk '{print $3}')"GB | Cpu: "(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')"" && echo ""
 end
 
 # configure display
-set -g theme_display_time                     yes
-set -g theme_display_group                    no
-set -g theme_display_git                      yes
-set -g theme_display_virtualenv               yes
-set -g theme_display_time_format              '+[%H:%M:%S]'
+set -g theme_display_time                   yes
+set -g theme_display_group                  no
+set -g theme_display_git                    yes
+set -g theme_display_virtualenv             yes
+set -g theme_display_time_format            '+[%H:%M]'
+set -gx fish_prompt_pwd_dir_length          1
 
 # config colours
-set -g theme_color_time                       brcyan
-set -g theme_color_user                       white
-set -g theme_color_separator                  white
-set -g theme_color_host                       brgreen
-set -g theme_color_bracket                    white
-set -g theme_color_normal                     white
-set -g theme_color_error                      brred
-set -g theme_color_superuser                  brred
-set -g theme_color_path                       brcyan
-set -g theme_color_rw                         white
-set -g theme_color_virtualenv                 bryellow
+set -g theme_color_time                     brcyan
+set -g theme_color_user                     white
+set -g theme_color_separator                white
+set -g theme_color_host                     brgreen
+set -g theme_color_bracket                  white
+set -g theme_color_normal                   white
+set -g theme_color_error                    brred
+set -g theme_color_superuser                brred
+set -g theme_color_path                     brcyan
+set -g theme_color_rw                       white
+set -g theme_color_virtualenv               bryellow
 
-set -g theme_prompt_char_normal               '$'
-set -g theme_prompt_chat_superuser            '#'
-set -g theme_prompt_char                      "$theme_prompt_chat_normal"
+# config chars
+set -g theme_prompt_char_normal             '$'
+set -g theme_prompt_chat_superuser          '#'
+set -g theme_prompt_char                    "$theme_prompt_chat_normal"
+set -g theme_prompt_userhost_separator      '@'
 
 # git config
-set -g __fish_git_prompt_char_branch_begin    '['
-set -g __fish_git_prompt_char_branch_end      ']'
-set -g __fish_git_prompt_color_branch_begin   white
-set -g __fish_git_prompt_color_branch_end     white
-set -g __fish_git_prompt_color_branch         bryellow
+set -g __fish_git_prompt_char_branch_begin  '['
+set -g __fish_git_prompt_char_branch_end    ']'
+set -g __fish_git_prompt_color_branch_begin white
+set -g __fish_git_prompt_color_branch_end   white
+set -g __fish_git_prompt_color_branch       bryellow
 
 # cmd aliases
 alias neofetch "neofetch --ascii_distro arch_old"
@@ -64,6 +69,9 @@ alias img "tiv -a -r 0.5"
 alias xmr "xmrig --donate-level 2 -o xmr-us-east1.nanopool.org:14433 -u 47z353DVtHkGTamcj7KYPtPsuCn481HAbL3uR2PLgxdQ2QYYJuwYmHcaQVRiFbGnaxDU2jKZTyMr15dx2HEuYhP471jsioW --tls --coin monero"
 alias handbrake "hbc"
 alias python "python3"
+alias pf "pfetch"
+# this seems stupid asf but is to leave additional padding at the top. dont @ me.
+alias clear "clear && echo ''"
 
 # add vue to path
 set -x PATH ~/.npm-global/bin:$PATH
