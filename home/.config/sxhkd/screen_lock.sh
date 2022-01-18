5b86e65fe90eb3ac2903c875149dc55c668beeba
kiosion <hi@kio.dev>
2022-01-18 14:06:45 -0400

    update stuffs

--- home/.config/sxhkd/screen_lock.sh
+++ home/.config/sxhkd/screen_lock.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
+rm -f /tmp/screen.png
+tmpbg = '/tmp/screen.png'
 
-#rm /tmp/screen.png
-#tmpbg='/tmp/screen.png'
+# take a screenshot, set as betterlockscreen's image
+scrot "$tmpbg" && betterlockscreen -u "$tmpbg" --fx blur
 
-# take a screenshot, size it down
-#scrot "$tmpbg" && convert "$tmpbg" -thumbnail 50% "$tmpbg"
-#convert "$tmpbg" -filter Gaussian -thumbnail 50% -sample 100% "$tmpbg" #-sample 500% "$tmpbg"
-
-# set betterlockscreen's image
-#betterlockscreen -u "$tmpbg" --fx blur
-
-# lock display with betterlockscreen
+# lock display
 betterlockscreen --lock blur
