20c8c7acafa92e3c0d88fa584c67f6748844cc56
kiosion <hi@kio.dev>
2022-01-25 20:03:47 -0400

    idfk random stuff

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -15,44 +15,50 @@
 fish_ssh_agent
 
 # greeter cmd for new windows
-if status is-interactive
-    echo "" && echo "Welcome, "(whoami)"! | Arch "(uname -sr)"" && echo ""
-    #echo "" && echo "Mem: "(free -g | sed -n 's/.*Mem://p' | awk '{print $3}')"GB | Cpu: "(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')"" && echo ""
-end
+#if status is-interactive
+    #echo "" && echo "Welcome, "(whoami)"! | Arch "(uname -sr)"" && echo ""
+#end
+
+# enable starship theme
+starship init fish | source
 
 # configure display
-set -g theme_display_time                   yes
-set -g theme_display_group                  no
-set -g theme_display_git                    yes
-set -g theme_display_virtualenv             yes
-set -g theme_display_time_format            '+[%H:%M]'
-set -gx fish_prompt_pwd_dir_length          1
+#set -g theme_display_time                   no
+#set -g theme_display_hostname               no
+#set -g theme_display_group                  no
+#set -g theme_display_git                    yes
+#set -g theme_display_virtualenv             yes
+#set -g theme_display_rw                     no
+#set -g theme_display_time_format            '+[%H:%M]'
+#set -gx fish_prompt_pwd_dir_length          1
 
 # config colours
-set -g theme_color_time                     brcyan
-set -g theme_color_user                     white
-set -g theme_color_separator                white
-set -g theme_color_host                     brgreen
-set -g theme_color_bracket                  white
-set -g theme_color_normal                   white
-set -g theme_color_error                    brred
-set -g theme_color_superuser                brred
-set -g theme_color_path                     brcyan
-set -g theme_color_rw                       white
-set -g theme_color_virtualenv               bryellow
+#set -g theme_color_time                     brcyan
+#set -g theme_color_user                     white
+#set -g theme_color_separator                white
+#set -g theme_color_host                     brgreen
+#set -g theme_color_bracket                  white
+#set -g theme_color_normal                   white
+#set -g theme_color_error                    brred
+#set -g theme_color_user                     brgreen
+#set -g theme_color_superuser                brred
+#set -g theme_color_path                     brcyan
+#set -g theme_color_prompt                   white
+#set -g theme_color_rw                       white
+#set -g theme_color_virtualenv               bryellow
 
 # config chars
-set -g theme_prompt_char_normal             '$'
-set -g theme_prompt_chat_superuser          '#'
-set -g theme_prompt_char                    "$theme_prompt_chat_normal"
-set -g theme_prompt_userhost_separator      '@'
+#set -g theme_prompt_char_normal             '→'
+#set -g theme_prompt_chat_superuser          '$'
+#set -g theme_prompt_char                    "$theme_prompt_chat_normal"
+#set -g theme_prompt_userhost_separator      '@'
 
 # git config
-set -g __fish_git_prompt_char_branch_begin  '['
-set -g __fish_git_prompt_char_branch_end    ']'
-set -g __fish_git_prompt_color_branch_begin white
-set -g __fish_git_prompt_color_branch_end   white
-set -g __fish_git_prompt_color_branch       bryellow
+#set -g __fish_git_prompt_char_branch_begin  'on ['
+#set -g __fish_git_prompt_char_branch_end    ']'
+#set -g __fish_git_prompt_color_branch_begin white
+#set -g __fish_git_prompt_color_branch_end   white
+#set -g __fish_git_prompt_color_branch       bryellow
 
 # cmd aliases
 alias neofetch "neofetch --ascii_distro arch --disk_show '/' '/mnt/kepler/' '/mnt/tesla/' --cpu_temp C"
@@ -73,21 +79,21 @@ alias pf "pfetch"
 #xmrig
 alias xmrigrun "~/.dotfiles/home/scripts/xmrigrun"
 
-# this seems stupid asf but is to leave additional padding at the top. dont @ me.
-alias clear "clear && echo ''"
-
 #yiffer-dl
 alias ydl "python ~/Nextcloud/Development/yiffer-dl/ydl.py"
+
+#thefuck
+thefuck --alias | source
 
 # add vue to path
 set -x PATH ~/.npm-global/bin:$PATH
 
 # if on tty1, launch startx automatically
-if status is-login
-    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        exec startx -- -keeptty
-    end
-end
+#if status is-login
+#    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+#        exec startx -- -keeptty
+#    end
+#end
 
 # load pyenv automatically
 # status is-interactive; and pyenv init --path | source
