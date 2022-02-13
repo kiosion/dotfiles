bf469d6b81c4db5f4a89e329b1853c35927a55ec
kiosion <hi@kio.dev>
2022-02-13 01:45:08 -0400

    add macos firefox config

---
+++ .macos/.config/firefox/user.js
@@ -0,0 +1,60 @@
+// Mozilla User Preferences
+
+// To change a preference value, you can either:
+// - modify it via the UI (e.g. via about:config in the browser); or
+// - set it within a user.js file in your profile.
+
+user_pref("accessibility.force_disabled", 1);
+user_pref("accessibility.typeaheadfind.flashBar", 0);
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.bookmarks.defaultLocation", "unfiled");
+user_pref("browser.bookmarks.editDialog.confirmationHintShowCount", 1);
+user_pref("browser.bookmarks.restore_default_bookmarks", false);
+user_pref("browser.bookmarks.showMobileBookmarks", false);
+user_pref("browser.cache.disk.filesystem_reported", 1);
+user_pref("browser.compactmode.show", true);
+user_pref("browser.contentblocking.category", "custom");
+user_pref("browser.download.panel.shown", true);
+user_pref("browser.formfill.enable", false);
+user_pref("browser.protections_panel.infoMessage.seen", true);
+user_pref("browser.search.region", "CA");
+user_pref("browser.search.suggest.enabled.private", true);
+user_pref("browser.startup.couldRestoreSession.count", 1);
+user_pref("browser.startup.page", 3);
+user_pref("browser.tabs.extraDragSpace", true);
+user_pref("browser.tabs.tabClipWidth", 83);
+user_pref("browser.theme.content-theme", 0);
+user_pref("browser.theme.toolbar-theme", 0);
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+user_pref("browser.uidensity", 1);
+user_pref("browser.urlbar.placeholderName", "DuckDuckGo");
+user_pref("browser.urlbar.placeholderName.private", "DuckDuckGo");
+user_pref("browser.urlbar.quicksuggest.migrationVersion", 2);
+user_pref("browser.urlbar.quicksuggest.scenario", "history");
+user_pref("browser.urlbar.suggest.engines", false);
+user_pref("browser.urlbar.suggest.history", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("browser.urlbar.tipShownCount.searchTip_onboard", 4);
+user_pref("browser.urlbar.tipShownCount.searchTip_redirect", 4);
+user_pref("browser.urlbar.tipShownCount.tabToSearch", 28);
+user_pref("browser.warnOnQuitShortcut", false);
+user_pref("devtools.everOpened", true);
+user_pref("fission.experiment.max-origins.qualified", true);
+user_pref("font.internaluseonly.changed", true);
+user_pref("full-screen-api.allow-trusted-requests-only", false);
+user_pref("general.smoothScroll.mouseWheel.migrationPercent", 0);
+user_pref("intl.accept_languages", "en");
+user_pref("media.autoplay.default", 5);
+user_pref("network.cookie.cookieBehavior", 1);
+user_pref("network.dns.disablePrefetch", true);
+user_pref("network.predictor.enabled", false);
+user_pref("network.prefetch-next", false);
+user_pref("pref.downloads.disable_button.edit_actions", false);
+user_pref("pref.privacy.disable_button.cookie_exceptions", false);
+user_pref("privacy.donottrackheader.enabled", true);
+user_pref("privacy.history.custom", true);
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("privacy.trackingprotection.socialtracking.enabled", true);
+user_pref("privacy.userContext.enabled", true);
+user_pref("privacy.userContext.ui.enabled", true);
+user_pref("security.insecure_connection_text.enabled", true);
