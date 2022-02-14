349257f0c59a750e2e8e806e8033037fed1c5127
kiosion <hi@kio.dev>
2022-02-14 16:17:11 -0400

    update scripts

---
+++ .config/ln.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# Nextcloud
+ln -s ./Nextcloud/sync-exclude.lst ~/.config/Nextcloud/sync-exclude.lst
+
+# alacritty
+ln -s ./alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -s ./alacritty/dracula.yml ~/.config/alacritty/dracula.yml
+
+# blender
+
+# bspwm
+ln -s ./bspwm/bspwmrc ~/.config/bspwm/bspwmrc
+
+# cava
+
+# doom emacs
+
+# fish
+ln -s ./fish/config.fish ~/.config/fish/config.fish
+ln -s ./fish/fish_plugins ~/.config/fish/fish_plugins
+ln -s ./fish/fish_variables ~/.config/fish/fish_variables
+
+# htop
+
+# hypr
+
+# nemo
+
+# neofetch
+
+# nitrogen
+
+# omf
+
+# pavucontrol
+ln -s ./pavucontrol.ini ~/.config/pavucontrol.ini
+
+# picom
+ln -s ./picom/picom.conf ~/.config/picom/picom.conf
+
+# polybar
+ln -s ./polybar/config ~/.config/polybar/config
+ln -s ./polybar/launch.sh ~/.config/polybar/launch.sh
+ln -s ./polybar/scripts/memory.sh ~/.config/polybar/scripts/memory.sh
+ln -s ./polybar/scripts/pkgs.sh ~/.config/polybar/scripts/pkgs.sh
+ln -s ./polybar/scripts/upt.sh ~/.config/polybar/scripts/upt.sh
+
+# powermgmt profiles
+ln -s ./powermanagementprofilesrc ~/.config/powermanagementprofilesrc
+
+
