d5076e78f388658a4d50b60acff40133e41496e5
kiosion <hi@kio.dev>
2022-03-26 22:14:44 -0300

    Made backup.sh general-purpose

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,35 +1,114 @@
 #!/bin/bash
 
-clear
-# Set path for backup location
-cwd="/mnt/kepler/Archives/"
-cd "${cwd}"
+# Help func 
+Help()
+{
+echo
+echo "Syntax: backup.sh -h, -s <source>, -d <dest>"
+echo
+}
+
+# Load func
+Load()
+{
+tput civis
+chars="/-\|"
+while :; do
+    for (( i=0; i<${#chars}; i++ )); do
+        sleep 0.1
+        echo -en "${chars:$i:1}" "\r"
+    done
+done
+}
+StopLoad()
+{
+kill "$!"
+tput cnorm
+}
+
+# Proccess input
+PASSED_ARGS=$@
+if [[ ${#PASSED_ARGS} -ne 0 ]]
+then
+    while getopts ":s:d:h" option; do
+        case $option in
+            h) # help
+                Help
+                exit;;
+            s) # source
+                srcDir=$OPTARG;;
+            d) # destination
+                dstDir=$OPTARG;;
+            :) # no arg given
+                echo "=> No arguments provided: Pass -h for help"
+                exit;;
+            \?) # invalid
+                echo "=> Invalid argument: Pass -h for help"
+                exit;;
+        esac
+    done
+else
+    Help
+    exit 1
+fi
+
+# If vars are empty
+if [[ -z "$srcDir" ]]
+then
+    echo "=> Missing required argument: -s"
+    exit 1
+fi
+
+if [[ -z "$dstDir" ]]
+then
+    echo "=> Missing required argument: -d"
+    exit 1
+fi
+
+echo "=> Source to backup: $srcDir"
+echo "=> Destination directory: $dstDir"
 # Set date
 curDate="$(date +"%d-%m-%Y")"
-# Set dir to backup
-bkDir="/home/kio/"
+# Remove trailing slash from dirs
+srcDir=$(sed -e 's#/$##' <<< $srcDir)
+dstDir=$(sed -e 's#/$##' <<<$dstDir)
+# Set filename
+fn=$(sed -e 's#^.*/##' <<< $srcDir)
+
+# Check directories are valid
+if [ ! -d "$srcDir" ]; then
+    echo
+    echo "Error: Source directory is invalid!"
+    echo
+    exit 1
+fi
+if [ ! -d "$dstDir" ]; then
+    echo
+    echo "Error: Destination directory is invalid!"
+    echo
+    exit 1
+fi
 
 # Ensure file doesn't already exist
-if [ -f "${cwd}homeBackup/home-backup.tar.gz" ]; then
-    echo ""
-    echo "=> Error: File already exists! Aborting..."
+if [ -d "${dstDir}/${curDate}/${fn}.tar.gz" ]; then
+    echo "=> Error: Destination already exists!"
     exit 1
 fi
 
 # Create dir if doesn't exist
-if [ -d "${cwd}homeBackup/" ]; then
-    echo ""
+if [ ! -d "${dstDir}/${curDate}/" ]; then
     echo "=> Creating directory for backup..."
-    mkdir -p "${cwd}homeBackup/"
-    cd "${cwd}homeBackup/"
-    sleep 1
+    mkdir -p "${dstDir}/${curDate}/"
+    cd "${dstDir}/${curDate}/"
 fi
 
 # Set source size
-size_source=`du -sb --exclude=.cache --exclude=.vscode/* --exclude=.local/share/Trash --exclude=Nextcloud /home/kio/ 2>/dev/null | awk '{print $1}'`
+echo "=> Calculating size..."
+Load &
+size_source=`du -sb --exclude=.cache --exclude=.vscode/* --exclude=.local/share/Trash --exclude=Nextcloud ${srcDir} 2>/dev/null | awk '{print $1}'`
 size_source_gb=`echo $size_source | awk '{print $1/1000/1000/1000}'`
+StopLoad
 echo "=> Total to backup: $size_source_gb GB"
-sleep 1
 
 # Use tar to create archive
 echo "=> Starting backup with tar..."
@@ -37,53 +116,11 @@ sleep 1
 clear
 
 # Run tar cmd
-echo ""
-tar cf - --ignore-failed-read -P --exclude=/home/kio/.cache --exclude=/home/kio/.local/share/Trash --exclude=Nextcloud /home/kio/ | pv -s ${size_source} | gzip > home-backup.tar.gz
+echo
+tar cf - --ignore-failed-read -P --exclude=/home/kio/.vscode --exclude=/home/kio/.cache --exclude=/home/kio/.local/share/Trash --exclude=Nextcloud ${srcDir} | pv -s ${size_source} | gzip > ${fn}.tar.gz
 
-# When finished creating archive
-clear
-echo ""
+# When finished
+echo
 echo "=> Archive creation complete!"
-sleep 1
-
-# Create directory on backup share
-remoteCwd="/mnt/volta/backups/Devices/Galileo/"
-echo ""
-echo "=> Creating destination directory..."
-if [ -d "${remoteCwd}${curDate}/" ]; then
-    echo ""
-    echo "=> Destination directory exists, skipping..."
-fi
-if [ ! -d "${remoteCwd}${curDate}/" ]; then
-echo "=> Creating destination directory..."
-    mkdir -P ${remoteCwd}${curDate}/
-    echo ""
-    echo "=> Destination directory created!"
-fi
-sleep 1
-clear
-
-# Copy file to backup SMB share
-echo ""
-echo "=> Preparing to copy archive with rsync..."
-
-# Set source size
-size_source=`du -bs ${cwd}homeBackup/ | sed -r "s/^[^0-9]*([0-9]+).*$/\1/"`
-size_source_gb=`echo $size_source | awk '{print $1/1000/1000/1000}'`
-echo "=> Total to copy: $size_source_gb GB"
-sleep 1
-clear
-# Run rsync cmd
-rsync -rtDq --stats --ignore-errors ${cwd}homeBackup/home-backup.tar.gz ${remoteCwd}${curDate}/home-backup.tar.gz | pv -s ${size_source}
-
-clear
-# Show progress
-#while [[ `jobs | grep "rsync"` ]]; do
-#    size_target=`du -bs $cwd/$curDate$bkDir 2>/dev/null | sed -r "s/^[^0-9]*([0-9]+).*$/\1/"`
-#    size_target_gb=`echo $size_target | awk '{print $1/1000/1000/1000}'`
-#    calc_done=`expr 100 \* $size_target / $size_source`
-#    echo "$"
-#done
-echo ""
-echo "=> Done!"
+echo
 exit 0
